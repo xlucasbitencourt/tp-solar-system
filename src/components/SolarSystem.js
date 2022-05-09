ad77fb78116c1c54f6a43d8ccb03b64b8a1ad036
Lucas Bitencourt <lblukz@aol.com>
2022-05-09 16:35:56 -0300

    Requisito 4 concluído

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Title from './Title'; // requisito 4
 
 function SolarSystem() { // requisito 2
   return (
-    <div data-testid="solar-system" />
+    <div data-testid="solar-system">
+      <Title headline="Planetas" />
+    </div>
   );
 }
 
