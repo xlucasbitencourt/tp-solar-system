b1fa523a3cb2c2b111404f4e70ad51c47ccbc502
Lucas Bitencourt <lblukz@aol.com>
2022-05-09 17:49:39 -0300

    Requisito 7 concluído

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header'; // requisito 1 - importar o arquivo para poder trabalhar com ele
 import SolarSystem from './components/SolarSystem'; // requisito 2
+import Missions from './components/Missions'; // requisito 7
 
 class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class App extends React.Component {
       <>
         <Header />
         <SolarSystem />
+        <Missions />
       </>
     );
   }
---
+++ src/components/Missions.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Missions() {
+  return (
+    <div data-testid="missions" />
+  );
+}
+
+export default Missions;
