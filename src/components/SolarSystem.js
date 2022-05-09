e8edc76360b2daaabc37d1cc87dba4bc7cdb85ee
Lucas Bitencourt <lblukz@aol.com>
2022-05-09 17:41:56 -0300

    Requisito 6 concluído

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Title from './Title'; // requisito 4
+import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 function SolarSystem() { // requisito 2
   return (
     <div data-testid="solar-system">
       <Title headline="Planetas" />
+      {planets.map((pl) => ( // Pega cada componente do array planets, e chama pra cada um o PlanetCard
+        <PlanetCard key={ pl.name } planetName={ pl.name } planetImage={ pl.image } />))}
     </div>
   );
 }
