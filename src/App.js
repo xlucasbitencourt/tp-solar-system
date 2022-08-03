69c19442c56627a757ed04b4ff2894b00c7bee63
Lucas Bitencourt <lblukz@aol.com>
2022-08-03 14:40:56 -0300

    Exclui arquivos desnecessários

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header'; // requisito 1 - importar o arquivo para poder trabalhar com ele
 import SolarSystem from './components/SolarSystem'; // requisito 2
 import Missions from './components/Missions'; // requisito 7
